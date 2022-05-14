6a67b515254327069565e9106e67a1d5a51e7548
alexcdever <alexcdever@gmail.com>
2022-05-15 02:58:01 +0800

    add unit test cases

--- test/butterfly_test.dart
+++ test/butterfly_test.dart
@@ -7,6 +7,18 @@ void main() {
     expect(generator.generate().toString().length, 19);
   });
 
+  test('get id generator by timestamp and machine number', () {
+    var generator = Butterfly.withTimestampAndMachineNumber(
+        DateTime.now().millisecondsSinceEpoch, 0);
+    expect(generator.generate().toString().length, 19);
+  });
+
+  test('test generate', () {
+    var generator = Butterfly.withTimestampAndMachineNumber(
+        DateTime.now().millisecondsSinceEpoch, 0);
+    expect(generator.generate().toString().length, 19);
+  });
+
   test('test batch generate', () {
     var generator = Butterfly(DateTime.now().millisecondsSinceEpoch, 0, 0, 0);
     int count = 10;
